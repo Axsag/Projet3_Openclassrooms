111d6f245184b4539971492b487dd4632baf5b87
Axsag <axel.saglier@gmail.com>
2018-04-15 00:39:49 +0200

    added backend theme

--- views/frontend/template.php
+++ views/frontend/template.php
@@ -11,15 +11,15 @@
     <title>Jean Forteroche</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="public/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="public/frontend/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
-    <link href="public/css/clean-blog.min.css" rel="stylesheet">
+    <link href="public/frontend/css/clean-blog.min.css" rel="stylesheet">
 
   </head>
 
@@ -113,11 +113,11 @@
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="public/vendor/jquery/jquery.min.js"></script>
-    <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="public/frontend/vendor/jquery/jquery.min.js"></script>
+    <script src="public/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="public/js/clean-blog.min.js"></script>
+    <script src="public/frontend/js/clean-blog.min.js"></script>
 
   </body>
 
